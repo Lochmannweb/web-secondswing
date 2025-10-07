c0c59c0789c2ffb02537d87abd46996b9f2fb666
Lochmannweb <lochmannweb@gmail.com>
2025-10-07 21:35:46 +0200

    page test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,35 @@
-
-
-import ForsideHero from "@/components/Forside";
+import Image from "next/image";
 import { Box } from "@mui/material";
+import ForsideHero from "@/components/Forside";
 
-export default async function Home() {
+export default function Home() {
   return (
-    <>
-        <div 
-          // style={{
-          //   height: "100vh", 
-          //   backgroundImage: `url(/golfbane.jpg)`,
-          //   backgroundSize: "cover",         
-          //   backgroundPosition: "center", 
-          //   marginTop: "0rem"
-          //   // marginTop: { xs: "0rem" } 
-          // }}
-          >
-            <image
-              style={{
-                height: "100vh", 
-                backgroundImage: `url(/golfbane.jpg)`,
-                backgroundSize: "cover",         
-                backgroundPosition: "center", 
-                marginTop: "0rem"
-                // marginTop: { xs: "0rem" } 
-              }}
-            />
-            <ForsideHero   />
-        </div>
-    </> 
+    <Box sx={{ position: "relative", height: "100vh", overflow: "hidden" }}>
+      {/* Optimized hero image */}
+      <Image
+        src="/golfbane.jpg"
+        alt="Golfbane"
+        fill
+        priority // ensures fast LCP load
+        style={{
+          objectFit: "cover",
+          objectPosition: "center",
+        }}
+      />
+
+      {/* Foreground content */}
+      <Box
+        sx={{
+          position: "relative",
+          zIndex: 2,
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <ForsideHero />
+      </Box>
+    </Box>
   );
 }
