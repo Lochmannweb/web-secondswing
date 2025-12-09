214ac2e6b219763de20869bdcee372655002b706
Lochmannweb <lochmannweb@gmail.com>
2025-12-09 15:54:16 +0100

    npm run build

---
+++ next.config.js
@@ -0,0 +1,29 @@
+const { withSentryConfig } = require('@sentry/nextjs');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bumycjssgayamirwtvxx.supabase.co',
+        pathname: '/storage/v1/object/public/avatars/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = withSentryConfig(nextConfig, {
+  org: 'line-lochmann-mller',
+  project: 'typescript-nextjs',
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  tunnelRoute: '/monitoring',
+  disableLogger: true,
+  automaticVercelMonitors: true,
+});
