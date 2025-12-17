b981b765b52349b117be76cd807fa50032321548
Lochmannweb <lochmannweb@gmail.com>
2025-12-17 20:52:47 +0100

    CSRF test

--- src/middelware.ts
+++ src/middelware.ts
@@ -5,24 +5,32 @@ export async function middleware(request: NextRequest) {
   // ----- CSRF VALIDERING (Origin/Referer) -----
   const method = request.method;
 
+  // Beskytter state-changerende requests (GET - må ikke ændre data)
   if (["POST", "PUT", "PATCH", "DELETE"].includes(method)) {
+
+    // Tjekker hvor requesten kommer fra
     const origin = request.headers.get("origin");
     const referer = request.headers.get("referer");
 
+    // Kun eget domæne er tilladt
     const allowed = process.env.NEXT_PUBLIC_BASE_URL;
 
     // hvis environment variabel mangler → skip CSRF-check
     if (!allowed) {
-      console.warn("CSRF: NEXT_PUBLIC_BASE_URL mangler");
+      console.error("CSRF: NEXT_PUBLIC_BASE_URL mangler");
       return NextResponse.json(
-        { error: "Server config error (missing NEXT_PUBLIC_BASE_URL)" },
+        { error: "Server configuration error" },
         { status: 500 }
       );
     }
 
+
+
+    // Requuesten er kun gyldig hvis enten origin eller referer matcher vores domæne
     const validOrigin = origin === allowed;
     const validReferer = referer?.startsWith(allowed) ?? false; // bliver automatisk kaldt hver gang typerne POST, PUT, PATCH, DELETE rammer serveren 
 
+    // Hvis requesten kommer fra et andet website - blokér den
     if (!validOrigin && !validReferer) {
       return new NextResponse("Forbidden – CSRF Blocked", { status: 403 });
     }
@@ -46,7 +54,7 @@ export async function middleware(request: NextRequest) {
       "style-src 'self' 'unsafe-inline' 'unsafe-eval' blob: 'wasm-unsafe-eval'",
 
       // Supabase API's + Google OAuth redirect
-      "connect-src 'self' https://*.supabase.co ht ts://accounts.google.com https://oauth2.googleapis.com",
+      "connect-src 'self' https://*.supabase.co htts://accounts.google.com https://oauth2.googleapis.com",
 
       // Next Image + Supabase storage 
       "img-src 'self'  blob: data: https://*.supabase.co",
@@ -63,10 +71,23 @@ export async function middleware(request: NextRequest) {
 
   
   // sikkerhes headers
-  response.headers.set("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload");
+  response.headers.set(
+    "Strict-Transport-Security", 
+    "max-age=63072000; includeSubDomains; preload"
+  );
+
   response.headers.set("X-Content-Type-Options", "nosniff");
-  response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
-  response.headers.set("Permissions-Policy", "Camera=(), microphone=(), geolocation=()");
+
+  response.headers.set(
+    "Referrer-Policy", 
+    "strict-origin-when-cross-origin"
+  );
+
+  response.headers.set(
+    "Permissions-Policy", 
+    "Camera=(), microphone=(), geolocation=()"
+  );
+
   response.headers.set("X-Frame-Options", "SAMEORIGIN");
 
   return response;
